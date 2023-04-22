2e1149e90a9d7976b1c9023c193e072c167332dd
lalukrish <lalukrishnavallil01@gmail.com>
2023-04-22 18:00:22 +0530

    feat: the image shows in the main page

--- src/canvas/Chess.jsx
+++ src/canvas/Chess.jsx
@@ -1,7 +1,26 @@
+import { Decal, useGLTF, useTexture } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { easing } from "maath";
 import React from "react";
+import { useSnapshot } from "valtio";
+import state from "../store";
 
 const Chess = () => {
-  return <div>Chess</div>;
+  const snap = useSnapshot(state);
+  const { nodes, materials } = useGLTF("/shirt_baked.glb");
+  const logoTexture = useTexture(snap.logoDecal);
+  const fullTexture = useTexture(snap.fullDecal);
+  return (
+    <group>
+      <mesh
+        castShadow
+        geometry={nodes.T_Shirt_male.geometry}
+        material={materials.lambert1}
+        material-roughness={1}
+        dispose={null}
+      ></mesh>
+    </group>
+  );
 };
 
 export default Chess;
--- src/canvas/index.jsx
+++ src/canvas/index.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 import { Canvas } from "@react-three/fiber";
 import { Environment, Center } from "@react-three/drei";
+import Chess from "./Chess";
+import CameraRig from "./CameraRig";
+import Backdrop from "./Backdrop";
 
 const CanvasModel = () => {
-  return <div>Canvas</div>;
+  return (
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <Environment preset="city" />
+      <CameraRig>
+        {/* <Backdrop /> */}
+        <Center />
+        <Chess />
+      </CameraRig>
+    </Canvas>
+  );
 };
 
 export default CanvasModel;
