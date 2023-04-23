cca130540e370fe8d586189995050ce75364ecc8
lalukrish <lalukrishnavallil01@gmail.com>
2023-04-23 12:24:33 +0530

    feat: Backdrop black color is coustamized

--- src/canvas/Backdrop.jsx
+++ src/canvas/Backdrop.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import { AccumulativeShadows, RandomizedLight } from "@react-three/drei";
+import React, { useRef } from "react";
 
 const Backdrop = () => {
-  return <div>Backdrop</div>;
+  const shadows = useRef();
+  return (
+    //shawods
+    <AccumulativeShadows
+      ref={shadows}
+      temporal
+      frames={60}
+      alphaTest={0.85}
+      scale={10}
+      rotation={[Math.PI / 2, 0, 0]}
+      position={[0, 0, -0.14]}
+    >
+      //black shadows behind the shirt postion and the light
+      <RandomizedLight
+        radius={9}
+        intensity={0.55}
+        ambient={0.25}
+        position={[5, 5, -9]}
+      />
+    </AccumulativeShadows>
+  );
 };
 
 export default Backdrop;
